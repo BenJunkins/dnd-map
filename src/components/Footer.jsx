617db9ab75380716a5c5e4910eb15445c7a15bc1
Benjamin Junkins <128095837+BenJunkins@users.noreply.github.com>
2026-03-20 11:20:07 -0400

    Update footer disclaimer text

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ export default function Footer() {
   return (
     <div className="footer-disclaimer">
       <p>
-        Faerun Monster Explorer is unofficial Fan Content permitted under the
+        Faerun Monster Atlas is unofficial Fan Content permitted under the
         Fan Content Policy. Not approved/endorsed by Wizards. Portions of the
         materials used are property of Wizards of the Coast. ©Wizards of the
         Coast LLC.
